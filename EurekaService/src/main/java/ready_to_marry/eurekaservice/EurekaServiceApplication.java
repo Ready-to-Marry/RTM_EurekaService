5f6bfc3a16df2239be5e40409b0b855adb7c27ef
Hyundoo <114637614+HyunDooBoo@users.noreply.github.com>
2025-05-01 14:50:26 +0900

    Eureka Server init

---
+++ EurekaService/src/main/java/ready_to_marry/eurekaservice/EurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package ready_to_marry.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+
+}
---
+++ EurekaService/src/test/java/ready_to_marry/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ready_to_marry.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
